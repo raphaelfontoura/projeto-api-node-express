4d38a3b05b245e2847b1bed60594861307952cdb
Raphael Fontoura <raphael.fontoura@gmail.com>
2024-09-12 11:00:30 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var apiRouterV1 = require('./routes/apiRouterV1')
+
+var app = express();
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1', apiRouterV1);
+app.use('/', indexRouter);
+
+module.exports = app;
